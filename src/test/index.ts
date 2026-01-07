e72d8c084566e9799c1fd930aef77682452d142d
MEGVII-INC\zoucheng02 <775106512@qq.com>
2026-01-07 20:49:31 +0800

    feat: add csv 1986

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,7 +1,7 @@
 import * as ani from "../index";
 
 document.documentElement.style.background = "#fff"; // #223
-const totalSec = 120;
+const totalSec = 300;
 const stage = new ani.Stage(undefined, totalSec);
 initStage(stage).then((stage) => {
   stage.play();
